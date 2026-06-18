6039a72257ac4331cf8e73f5094907bb49ea861d
rodr8go <rodrigo.1704200@gmail.com>
2026-06-18 09:27:24 +0100

    Delete TorneoApp directory

--- TorneoApp/src/test/java/torneoapp/junit/Test.java
+++
@@ -1,6 +0,0 @@
-package torneoapp.junit;
-import java.lang.annotation.*;
-/** Marca um método como teste unitário. */
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
-public @interface Test {}
